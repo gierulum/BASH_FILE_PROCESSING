a552dc8ee049feaf95bc8baec9c936e0860d95e7
gierulum <marcin.gierulewicz@gmail.com>
2018-08-19 11:07:21 +0200

    commit new_batch 2018/08/19
    changed variables, update some operations processes and add some commends

--- run_scripts.bat
+++ run_scripts.bat
@@ -1,22 +1,30 @@
 @echo off
 setlocal EnableDelayedExpansion
-REM deklaracja o jako numer pozycji w arrays.
+REM declae o as numbers of arrays:
 set o=0
-for %%I in ("C:\DOCUMENTS\BAT_OPERATIONS\BAT_FILES\*") do (
+REM create list of files for number of operations:
+for %%I in ("E:\DEVELOP\GITHUB\BASH_FILE_PROCESSING\BASH_FILE_PROCESSING\BAT_FILES\*") do (
 set /A o=o+1 
 set lista_plikow_nazwy[!o!]=%%I
 set lista_plikow_imiona[!o!]=%%~nI
 set lista_plikow_daty[!o!]=%time:~0,2%:%time:~3,2%
-REM lista plikow po nazwach i sortowanie:
-for /f "delims== tokens=2" %%x in ('set lista_plikow_nazwy') do (
-echo %%x | sort /R >> file_operations.txt
+echo %%I >> file_operations.txt
+)
+REM sort files list:
+for /f %%G in (file_operations.txt) do (
+REM sort list of files in another file:
 sort /R file_operations.txt /o file_operations_reverse.txt)
 )
-REM proces zapisywania danych i przenoszenia plikow do innych folderow:
-for /f %%G in (file_operations_reverse.txt) do (
-for /f "tokens=*" %%L in (%G%) do (
-sqlldr LOGIN/PASSWORD@CONNCTION_DATABASE control=ctl_for_bat.ctl log=log_for_bat.log)
-move %%G C:\DOCUMENTS\BAT_OPERATIONS\BAT_DONE
-echo plik %%~nxG: został przeprocesowany: %DATE% %time:~0,2%:%time:~3,2% >> E:\DEVELOP\BATSCHER\proces_log_file.txt)
+REM create process of changing directory for files:
+for /f %%W in (file_operations_reverse.txt) do (
+REM change directory of files:
+move %%W E:\DEVELOP\GITHUB\BASH_FILE_PROCESSING\BASH_FILE_PROCESSING\BAT_DONE
+REM could create new names of files (if not unique names at in time) in line with variety of:
+REM echo %%~nxW %DATE% %time:~0,2%:%time:~3,2% >> file_operations.txt
+REM echo information of succesfully files process:
+echo plik %%~nxW: has been processed: %DATE% %time:~0,2%:%time:~3,2% >> E:\DEVELOP\GITHUB\BASH_FILE_PROCESSING\BASH_FILE_PROCESSING\proces_log_file.txt)
 )
+REM insert data to database as user GIER:
+REM create table aa_bat_test(id number(10),name varchar2(10) );
+sqlldr GIER/Leopold666@ORCL control=ctl_for_bat.ctl log=log_for_bat.log
 pause
